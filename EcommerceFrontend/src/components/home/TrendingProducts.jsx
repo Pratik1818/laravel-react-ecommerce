1df6df243b60306ebaf1a765fd81198a65dfc20a
swapnilkhartode <swapkhartode0891@gmail.com>
2025-09-04 20:59:40 +0530

    added product and category images

--- EcommerceFrontend/src/components/home/TrendingProducts.jsx
+++ EcommerceFrontend/src/components/home/TrendingProducts.jsx
@@ -1,26 +1,53 @@
 import React from "react";
 
-const products = [
-  { name: "Smartwatch", price: "$199.99", img: "/images/watch.jpg" },
-  { name: "Sneaker", price: "$79.99", img: "/images/sneaker.jpg" },
-  { name: "Handbag", price: "$129.99", img: "/images/handbag.jpg" },
-  { name: "Wireless On", price: "$99.99", img: "/images/headphone.jpg" },
-];
+import "../../assets/styles/product.css";
+import handbagsImg from "../../assets/images/products/handbags.jpg";
+import smartwatchImg from "../../assets/images/products/smartwatch.jpg";
+import sneakersImg from "../../assets/images/products/sneakers.jpg";
+import wireless_onImg from "../../assets/images/products/wireless_on.jpg";
 
 function TrendingProducts() {
   return (
     <section className="container my-5">
       <h2 className="text-center mb-4">Trending Products</h2>
       <div className="row text-center">
-        {products.map((product, index) => (
-          <div className="col-md-3" key={index}>
-            <div className="card p-3">
-              <img src={product.img} alt={product.name} className="img-fluid" />
-              <h5 className="mt-3">{product.name}</h5>
-              <p>{product.price}</p>
-            </div>
+
+        {/* Smartwatch */}
+        <div className="col-md-3">
+          <div className="card p-3">
+            <img src={smartwatchImg} alt="Smartwatch" className="product-img" />
+            <h5 className="mt-3">Smartwatch</h5>
+            <p>$199.99</p>
           </div>
-        ))}
+        </div>
+
+        {/* Sneaker */}
+        <div className="col-md-3">
+          <div className="card p-3">
+            <img src={sneakersImg} alt="Sneaker" className="product-img" />
+            <h5 className="mt-3">Sneaker</h5>
+            <p>$79.99</p>
+          </div>
+        </div>
+
+        {/* Handbag */}
+        <div className="col-md-3">
+          <div className="card p-3">
+            <img src={handbagsImg} alt="Handbag" className="product-img" />
+            <h5 className="mt-3">Handbag</h5>
+            <p>$129.99</p>
+          </div>
+        </div>
+
+        {/* Wireless On (Headphones) */}
+        <div className="col-md-3">
+          <div className="card p-3">
+            <img src={wireless_onImg} alt="Wireless On" className="product-img" />
+            <h5 className="mt-3">Wireless On</h5>
+            <p>$99.99</p>
+          </div>
+        </div>
+
       </div>
     </section>
   );
