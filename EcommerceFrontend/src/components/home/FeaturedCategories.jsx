ee3a790d57e34300e0060158e81304a953292c2c
swapnilkhartode <swapkhartode0891@gmail.com>
2025-09-04 20:43:33 +0530

    images added

--- EcommerceFrontend/src/components/home/FeaturedCategories.jsx
+++ EcommerceFrontend/src/components/home/FeaturedCategories.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import "../../assets/styles/category.css";
 
-const categories = [
-  { name: "Electronics", img: "../../assets/images/categories/electronics.jpg" },
-  { name: "Fashion", img: "../../assets/images/categories/fashion.jpg" },
-  { name: "Home & Kitchen", img: "../../assets/images/categories/home&kitchen.jpg" },
-  { name: "Beauty", img: "../../assets/images/categories/beauty.jpg" },
-];
+import electronicsImg from "../../assets/images/categories/electronics.jpg";
+import fashionImg from "../../assets/images/categories/fashion.jpg";
+import homeKitchenImg from "../../assets/images/categories/home_kitchen.jpg";
+import beautyImg from "../../assets/images/categories/beauty.jpg";
 
 function FeaturedCategories() {
   return (
     <section className="container my-5">
       <h2 className="text-center mb-4">Featured Categories</h2>
       <div className="row text-center">
-        {categories.map((cat, index) => (
-          <div className="col-md-3" key={index}>
-            <div className="category-card">
-              <img src={cat.img} alt={cat.name} className="img-fluid" />
-              <p>{cat.name}</p>
-            </div>
+        
+        {/* Electronics */}
+        <div className="col-md-3">
+          <div className="category-card">
+            <img src={electronicsImg} alt="Electronics" className="img-fluid" />
+            <p>Electronics</p>
           </div>
-        ))}
+        </div>
+
+        {/* Fashion */}
+        <div className="col-md-3">
+          <div className="category-card">
+            <img src={fashionImg} alt="Fashion" className="img-fluid" />
+            <p>Fashion</p>
+          </div>
+        </div>
+
+        {/* Home & Kitchen */}
+        <div className="col-md-3">
+          <div className="category-card">
+            <img src={homeKitchenImg} alt="Home & Kitchen" className="img-fluid" />
+            <p>Home & Kitchen</p>
+          </div>
+        </div>
+
+        {/* Beauty */}
+        <div className="col-md-3">
+          <div className="category-card">
+            <img src={beautyImg} alt="Beauty" className="img-fluid" />
+            <p>Beauty</p>
+          </div>
+        </div>
+
       </div>
     </section>
   );
