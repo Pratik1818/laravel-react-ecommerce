457aa400c3f32a7152c95f76db15d9c8b8bb9f7c
Pratik1818 <pratikchavan20191@gmail.com>
2025-09-09 21:04:04 +0530

    Added AppRoutes.jsx

--- EcommerceFrontend/src/routes/AppRoutes.jsx
+++ EcommerceFrontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import ProductDetails from '../pages/ProductDetails';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/productdetails" element={<ProductDetails />} />
+      <Route path="/login" element={<Login />} />
+       <Route path="/register" element={<Register/>} />
 
       </Routes>
       
